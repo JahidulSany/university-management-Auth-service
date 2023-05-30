f996f0867f7d37856a445c79a36ea4c43d5e2980
JahidulSany <jahidulsanypro@gmail.com>
2023-05-30 12:56:07 +0100

    [Done] Setup express, mongoose, cors, and bodyparser

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config/index';
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log(`🛢️ Database is connected successfully!!`);
+
+    app.listen(config.port, () => {
+      console.log(`Application is running on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(`failed to connect Database, ${err}`);
+  }
+}
+
+main();
