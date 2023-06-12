4b579d78c0aa734df3c9ef51142697dd3eddc853
JahidulSany <jahidulsanypro@gmail.com>
2023-06-12 11:40:13 +0100

    [Done] Academic Semester Constant

--- src/app/modules/academicSemester/academicSemester.interface.ts
+++ src/app/modules/academicSemester/academicSemester.interface.ts
@@ -1,11 +1,27 @@
 import { Model } from 'mongoose'
+export type Month =
+  | 'January'
+  | 'February'
+  | 'March'
+  | 'April'
+  | 'May'
+  | 'June'
+  | 'July'
+  | 'August'
+  | 'September'
+  | 'October'
+  | 'November'
+  | 'December'
+
+export type Title = 'Autumn' | 'Summer' | 'Fall'
+export type Code = '01' | '02' | '03'
 
 export type IAcademicSemester = {
-  title: string
+  title: Title
   year: number
-  code: string
-  startMonth: string
-  endMonth: string
+  code: Code
+  startMonth: Month
+  endMonth: Month
 }
 
 export type AcademicSemesterModel = Model<IAcademicSemester>
